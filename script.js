202bf228ced54f918c0e62908e93611de2a96070
commblock <aj.shevch@gmail.com>
2026-08-07 14:18:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const data = [
+    ["Aviasales", "infinity", "infinity"],
+    ["USA", 22500400, 3170],
+    ["Liam", 5500200, 1010],
+    ["Veronica A.", 2005000, 1000],
+    ["Verinica T.", 2067000, 1000],
+    ["Yura", 6099400, 1465],
+    ["Sean", 6084400, 995],
+    ["Zlata", 22002500, 1197],
+    ["Sasha", 5996000, 1807],
+    ["Gabby", 2002200, 2057],
+    ["Faina", "undefined", 995]
+];
+
+const tbody = document.querySelector("#myTable tbody");
+tbody.innerHTML = data.map(row => 
+    `<tr>${row.map(cell => `<td>${cell}</td>`).join('')}</tr>`
+).join('');
